f997375254346e71037688d61f4b8bfabc644fcb
p0ngdang <87624493+p0ngdang@users.noreply.github.com>
2022-06-01 10:25:06 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = rate/100;
+    var amount = (principal * years) * interest;
+    var year = new Date().getFullYear()+parseInt(years);
+    document.getElementById("result").innerHTML="If you deposit <mark>"+principal+"</mark>,\<br\>at an interest rate of <mark>"+rate+"%</mark>\<br\>You will receive an amount of <mark>"+amount+"</mark>,\<br\>in the year <mark>"+year+"</mark>\<br\>"
+
+
+    if(principal<=0)
+    {
+        alert("Enter a positive number");
+        
+
+        return false;
+    }
+
 }
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval+"%";
+}
+
         
